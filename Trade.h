4ecf83a2d7901cd646e5ab7ba041466f5a1bd04b
Khalil Ahmed <mkhalila@outlook.com>
2016-12-14 17:00:19 +0000

    Initial non-working attempt - better than nothing

--- Trade.h
+++ Trade.h
@@ -7,8 +7,25 @@
 using std::vector;
 
 template <typename T>
-int bestProfit(T & begin, T & end) {
+int bestProfit(T begin, T end) {
+    if (begin - end == 2) {
+        auto first = *begin;
+        ++begin;
+        auto second = *begin;
+        return first - second;
+    }
 
+    else if(begin - end == 1) return *begin;
+
+    else {
+        int distance = std::distance(begin, end)/2;
+        auto middle = begin + distance;
+        int firstHalf = bestProfit(begin, middle);
+        int secondHalf = bestProfit(middle, end);
+
+        if (firstHalf > secondHalf) return firstHalf;
+        else return secondHalf;
+    }
 }
 
 
